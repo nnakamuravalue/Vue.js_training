953e9fa580f10ef49816ee3f71adca3994009604
仲村宣道 <nakamura@value-it.co.jp>
2020-09-28 13:19:49 +0900

    Vue.js training

---
+++ vue-ts-practice/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <MyButton></MyButton>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+// import HelloWorld from '@/components/HelloWorld.vue';
+import MyButton from "@/components/MyButton.vue"; // @ is an alias to /src
+
+@Component({
+  components: {
+    MyButton
+  },
+})
+export default class Home extends Vue {}
+</script>
