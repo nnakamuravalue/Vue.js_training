f0e9e03629ab17bc2608be7649390b1e20f8981c
仲村宣道 <nakamura@value-it.co.jp>
2020-09-30 17:39:18 +0900

    Unit Test

--- vue-image-placeholder/tests/unit/imageplacerholder.spec.ts
+++ vue-image-placeholder/tests/unit/imageplacerholder.spec.ts
@@ -3,11 +3,20 @@ import ImagePlaceholder from '@/ImagePlaceholder.vue'
 
 describe('ImagePlaceholder.vue', () => {
   it('renders the correct url for New York images', () => {
-    const wrapper = shallowMount(ImagePlaceholder, {
+    const sut = shallowMount(ImagePlaceholder, {
       propsData: { width: 500, height:200, images: 'newyork' }
     })
     expect(
-        wrapper.findAll('img').at(0).attributes().src
+        sut.findAll('img').at(0).attributes().src
     ).toEqual('https://loremflickr.com/500/200/newyork')
+  })
+
+  it('renders the correct url for Random images if not specified', () => {
+    const sut = shallowMount(ImagePlaceholder, {
+      propsData: {width: 500, height: 200}
+    })
+    expect(
+        sut.findAll('img').at(0).attributes().src)
+        .toEqual('https://loremflickr.com/500/200/random')
   })
 })
