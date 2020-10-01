9ce52c3abc1aca92df20a127c86608a636c93631
仲村宣道 <nakamura@value-it.co.jp>
2020-10-01 13:06:14 +0900

    To do list with Vue.ts

--- vue-todo-list/src/router/index.ts
+++ vue-todo-list/src/router/index.ts
@@ -8,20 +8,12 @@ const routes: Array<RouteConfig> = [
   {
     path: '/',
     name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import('../views/home/home.component.vue')
   }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: 'hash',
   base: process.env.BASE_URL,
   routes
 })
