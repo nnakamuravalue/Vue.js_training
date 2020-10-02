864252fd3b22b124f73f5da0ae7ff3a61178e049
仲村宣道 <nakamura@value-it.co.jp>
2020-10-02 10:13:44 +0900

    To do list with Vue.ts

--- vue-todo-list/src/store/index.ts
+++ vue-todo-list/src/store/index.ts
@@ -1,15 +1,33 @@
 import Vue from 'vue'
-import Vuex from 'vuex'
+import Vuex, {StoreOptions} from 'vuex'
+import {Result} from "@/store/result";
+import {Task} from "@/models/task.model";
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
+const store: StoreOptions<Result<Task[]>> = {
   state: {
+    success: true,
+    message: undefined,
+    errorCode: 200,
+    version: '1.0.0',
+    data: new Array<Task>(),
   },
   mutations: {
+    addTask(state, task: Task) {
+      state.data.push(task)
+    },
+    editTask(state, task: Task) {
+      let taskIndex: number = state.data.findIndex(t => t.id == task.id)
+      state.data[taskIndex] = task;
+    },
+    removeTask(state, task: Task) {}
+    let taskIndex: number = state.data.findIndex
   },
   actions: {
   },
-  modules: {
+  getters: {
   }
-})
+}
+
+export default new Vuex.Store(store)
--- vue-todo-list/src/store/result.ts
+++ vue-todo-list/src/store/result.ts
@@ -1,5 +1,7 @@
 export interface Result<T> {
     success: boolean;
     message?: string;
-
+    errorCode?: number;
+    version?: string;
+    data: T;
 }
